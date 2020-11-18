76a70be0eb376ae7404b8dbe54cfd56b76bd2f2a
Felix Lemke <flemke.dev@gmail.com>
2020-11-18 19:47:10 +0100

    tests: add chromeheadlessci to karma config

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromeHeadlessCI'],
     singleRun: false,
     restartOnFileChange: true
   });
